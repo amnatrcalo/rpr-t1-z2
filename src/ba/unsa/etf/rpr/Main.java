06e2cd6b470f8c76d9b1c73110cda88f520af6f9
Amna Trcalo <atrcalo1@etf.unsa.ba>
2020-10-26 18:30:21 +0100

    Zadatak zavrsen

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int SumaCifara (int n){
+        int suma=0;
+        do{
+            int zadnjacifra=n%10;
+            suma+=zadnjacifra;
+            n/=10;
+        }while(n!=0);
+        return suma;
+    }
+
+    public static void main(String[] args) {
+	Scanner ulaz=new Scanner(System.in);
+        System.out.println("Unesi broj n: ");
+        int n=ulaz.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%SumaCifara(i)==0) System.out.println(i);
+        }
+    }
+}
